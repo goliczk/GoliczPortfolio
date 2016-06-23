90950b7508b0418a6c0387d8f6c6f1288eded48a
goliczk <goliczk@gmail.com>
2016-06-23 21:48:38 +0000

    Add files via upload

---
+++ PortfolioJavaScript.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+function clicked() {
+    document.getElementById("name").innerHTML=== "name";
+	if (clicked)
+	{console.log("Thank you" + " " + "name");
+	}
+	else
+{ console.log("Thank you");
+}
