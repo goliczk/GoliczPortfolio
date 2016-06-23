066824068f4b7ef709dcf2a0483e9b7e4f4541d7
goliczk <goliczk@gmail.com>
2016-06-23 17:46:14 -0400

    Delete PortfolioJavaScript[1].js

--- PortfolioJavaScript[1].js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-var clientName = prompt("What is your name?", "Text goes here");
-if (clientName!== null) {
-	document.getElementById("form").innerHTML =
-    "Hello " + clientName;
-}
